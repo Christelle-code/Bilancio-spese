18f634b759829718dc0d10212a037da075c79caa
Denise Guacci <116224101+Christelle-code@users.noreply.github.com>
2024-02-27 01:12:11 +0100

    Login e register completi, finire responsive add-transaction component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { AddTransactionComponent } from './add-transaction/add-transaction.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    AddTransactionComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
